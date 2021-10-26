e8c5794b285725494ea996d897224bee8a6c19db
Shahrear Rahman <shahrear@Shahrears-MacBook-Air.local>
2021-10-26 08:16:28 +0600

    Home component texts & Funds added to home and nav

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>Home Component</h1>
+    <div>
+        <h1>Trade or View your Portfolio</h1>
+        <p>You may Save & Load your data</p>
+        <p>Click on 'End Day' to begin a New Day</p>
+        <hr>
+        <p>Your Funds: {{ funds }}</p>
+    </div>
 </template>
+
+<script>
+    export default {
+        computed: {
+            funds() {
+                return this.$store.getters.funds
+            }
+        }
+    }
+</script>
